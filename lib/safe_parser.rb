5a5e8ce2cf1f6384013534dedbb73fb61a6bd5f1
Lucas Uyezu <lucas.uyezu@shopify.com>
2017-03-31 17:08:01 -0400

    Let's actually use this

--- lib/safe_parser.rb
+++ lib/safe_parser.rb
@@ -2,7 +2,7 @@ require "ruby_parser"
 
 # Whiltelist based hash string parser
 class SafeParser
-  VERSION = "0.0.2"
+  VERSION = "1.0.0"
 
   UnsafeError = Class.new(StandardError)
   attr_reader :string
